e824d796750a3bed0b2e070de85ee06295ed8855
heyYuvi <prasadyuvraj39@gmail.com>
2026-08-18 16:17:34 +0530

    Add design to Create and Update pages

--- frontend/src/pages/CreateTask.jsx
+++ frontend/src/pages/CreateTask.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import api from "../services/api";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { CiCircleCheck } from "react-icons/ci";
+
 
 const CreateTask = () =>{
 
@@ -35,24 +37,38 @@ const CreateTask = () =>{
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                Title <input type="text" name="title" value={form.title} onChange={handleChange} />
-                Description <input type="text" name="description" value={form.description} onChange={handleChange} />
-                Due Date <input type="date" name="dueDate" value={form.dueDate} onChange={handleChange} /> 
-                Status:
-                <select name="status" value={form.status} onChange={handleChange}>
+        <div className="w-full max-w-4xl m-auto p-4 mt-10 border border-gray-300 rounded-md">
+            <div className="mb-4">
+                <h1 className="font-semibold text-2xl">Create New Task</h1>
+                <h2 className="text-gray-600">Fill in the details below to add a new task to your workflow.</h2>
+            </div>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-2">
+                <div className="flex flex-col gap-1">
+                <label htmlFor="title">TASK TITLE</label>
+                <input type="text" id="title" name="title" value={form.title} onChange={handleChange} className="border rounded-md border-gray-500 focus:outline-none px-2 focus:ring-0"/>
+                </div>
+                <div className="flex flex-col gap-1">
+                <label htmlFor="description">DESCRIPTION</label>
+                <textarea id="description" name="description" value={form.description} onChange={handleChange} className="border rounded-md border-gray-500 w-full h-32"/>
+                </div>
+                <div className="grid grid-cols-[auto_auto] gap-8">
+                    
+                <select name="status" value={form.status} onChange={handleChange} className="border p-2 rounded-md border-gray-300">
                     <option value="pending">Pending</option>
                     <option value="in-progress">In-Progress</option>
                     <option value="completed">Completed</option>
                 </select>
-                Priority:
-                <select name="priority" value={form.priority} onChange={handleChange}>
+                <select name="priority" value={form.priority} onChange={handleChange}  className="border p-2 rounded-md border-gray-300">
                     <option value="low">Low</option>
                     <option value="medium">Medium</option>
                     <option value="high">High</option>
                 </select>
-                <button>Create Task</button>
+                </div>
+                <input type="date" name="dueDate" value={form.dueDate} onChange={handleChange} className="border w-55 p-2 rounded-md border-gray-300"/> 
+                <div className="flex gap-4 self-end mt-6">
+                    <button type="button" onClick={() =>{navigate("/")}} className="border p-2 rounded-md border-gray-400 bg-gray-100 hover:bg-gray-400 cursor-pointer transition">Cancel</button>
+                    <button className="flex items-center gap-1 border font-bold text-white p-2 rounded-md bg-blue-600 hover:bg-blue-400"><CiCircleCheck size={20}/>Create Task</button>
+                </div>
             </form>
         </div>
     )
--- frontend/src/pages/EditTask.jsx
+++ frontend/src/pages/EditTask.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom"
 import api from "../services/api";
 import toast from "react-hot-toast";
+import { FaFileSignature } from "react-icons/fa";
+
 
 const EditTask = () => {
 
@@ -17,11 +19,20 @@ const EditTask = () => {
         createdAt: "",
         updatedAt: ""
     });
+    console.log(form);
 
     useEffect(() => {
         const fetchTask = async () => {
             const response = await api.get(`/task/${id}`);
-            setForm(response.data.data)
+            setForm({
+                title: response.data.data.title,
+                description: response.data.data.description,
+                status: response.data.data.status,
+                priority: response.data.data.priority,
+                dueDate: response.data.data.dueDate ? response.data.data.dueDate.slice(0, 10) : "",
+                createdAt: response.data.data.createdAt,
+                updatedAt: response.data.data.updatedAt
+            });
         }
 
         fetchTask();
@@ -36,41 +47,66 @@ const EditTask = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        try{
+        try {
             await api.put(`/task/${id}`, form);
-        toast.success("Updated Successfully");
-        setForm({
-            title: "",
-            description: "",
-            status: "",
-            priority: "",
-            dueDate: "",
-            createdAt: "",
-            updatedAt: ""
-        });
-        
-        navigate("/");
-        }catch(error){
+            toast.success("Updated Successfully");
+            setForm({
+                title: "",
+                description: "",
+                status: "",
+                priority: "",
+                dueDate: "",
+                createdAt: "",
+                updatedAt: ""
+            });
+
+            navigate("/");
+        } catch (error) {
             toast.error(error.response?.data?.message || "Somthing Went Wrong");
         }
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                title: <input type="text" name="title" value={form.title} onChange={handleChange} />
-                description: <input type="text" name="description" value={form.description} onChange={handleChange} />
-                <select name="status" value={form.status} onChange={handleChange}>
-                    <option value="pending">Pending</option>
-                    <option value="in-progress">In-Progress</option>
-                    <option value="completed">Completed</option>
-                </select>
-                <select name="priority" value={form.priority} onChange={handleChange}>
-                    <option value="low">Low</option>
-                    <option value="medium">Medium</option>
-                    <option value="high">High</option>
-                </select>
-                <button>Update</button>
+        <div className="w-full max-w-4xl h-screen m-auto mt-10">
+            <div className="flex items-center justify-between">
+                <div className="mb-8">
+                    <h1 className="text-3xl font-bold mb-1">Update Task</h1>
+                    <h2 className="text-gray-500">Modify the details of your existing task or remove it entirely.</h2>
+                </div>
+                <div className="text-gray-500">Last Updated At {new Date(form.updatedAt).toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "2-digit",
+                    year: "numeric"
+                })}</div>
+            </div>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-full max-w-3xl m-auto p-4 bg-blue-100 rounded-2xl">
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="title" className="font-semibold">Task Title</label>
+                    <input type="text" id="title" name="title" value={form.title} onChange={handleChange} className="bg-white rounded-md px-4 py-2 focus:outline-none" />
+
+                </div>
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="description" className="font-semibold">Description (Optional)</label>
+                    <textarea id="description" name="description" value={form.description} onChange={handleChange} className="bg-white w-full h-32 px-4 py-2 rounded-2xl" />
+
+                </div>
+                <div className="grid grid-cols-[auto_auto_auto] gap-4 p-1">
+                    <input type="date" name="dueDate" value={form.dueDate} onChange={handleChange} className="bg-white p-1 rounded-md" />
+                    <select name="status" value={form.status} onChange={handleChange} className="bg-white p-1 rounded-md">
+                        <option value="pending">Pending</option>
+                        <option value="in-progress">In-Progress</option>
+                        <option value="completed">Completed</option>
+                    </select>
+                    <select name="priority" value={form.priority} onChange={handleChange} className="bg-white p-1 rounded-md">
+                        <option value="low">Low</option>
+                        <option value="medium">Medium</option>
+                        <option value="high">High</option>
+                    </select>
+                </div>
+                <div className="flex gap-5 self-end mt-4">
+                    <button type="button" onClick={() => { navigate("/") }} className="font-semibold cursor-pointer">Cancel</button>
+                    <button className="flex items-center gap-1 font-semibold p-2 bg-blue-600 text-white rounded-md hover:bg-blue-500 cursor-pointer transition"><FaFileSignature />Update Task</button>
+                </div>
             </form>
         </div>
     )
